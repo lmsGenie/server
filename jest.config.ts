8f94cfa6b1d55bae574719b2c8c7cd32f554dd5b
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-08-29 16:07:11 +0530

    feat: setup typescript, jest with TS, and add nvmrc

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'], // To ignore tests from the dist/ directory
+};
