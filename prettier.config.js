339e3704f3a1bbeef968cad57495916b1007618e
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-08-29 16:07:47 +0530

    feat: setup husky, commitlint, commitizen, lint-staged, and prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,6 @@
+// @ts-check
+
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
   trailingComma: 'all',
   printWidth: 80,
@@ -5,4 +8,20 @@ module.exports = {
   useTabs: false,
   semi: true,
   singleQuote: false,
+  importOrder: [
+    '<BUILTIN_MODULES>', // Node.js built-in modules
+    '',
+    '<THIRD_PARTY_MODULES>', // Imports not matched by other special words or groups.
+    '',
+    '^@core/(.*)$',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
 };
