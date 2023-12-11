7173701d9f5f8b947f56eab36ff57708b62b550a
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-12-11 22:52:37 +0530

    feat: add timestamps, made isActive required in coupon schema

--- src/models/coupon.model.ts
+++ src/models/coupon.model.ts
@@ -6,27 +6,33 @@ export interface ICoupon extends Document {
   isActive: boolean;
 }
 
-const couponSchema = new Schema<ICoupon>({
-  couponCode: {
-    type: String,
-    required: [true, "Coupon code is required"],
-    minlength: [6, "Coupon code must be at least 6 characters"],
-    maxlength: [25, "Coupon code cannot exceed 25 characters"],
-    uppercase: true,
-    unique: true,
-    trim: true,
+const couponSchema = new Schema<ICoupon>(
+  {
+    couponCode: {
+      type: String,
+      required: [true, "Coupon code is required"],
+      minlength: [6, "Coupon code must be at least 6 characters"],
+      maxlength: [25, "Coupon code cannot exceed 25 characters"],
+      uppercase: true,
+      unique: true,
+      trim: true,
+    },
+    discountPercentage: {
+      type: Number,
+      required: [true, "Discount percentage is required"],
+      min: [0, "Discount percentage must be atleast 0"],
+      max: [100, "Discount percentage cannot be more than 100"],
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
   },
-  discountPercentage: {
-    type: Number,
-    required: [true, "Discount percentage is required"],
-    min: [0, "Discount percentage must be atleast 0"],
-    max: [100, "Discount percentage cannot be more than 100"],
+  {
+    timestamps: true,
   },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-});
+);
 
 const CouponModel = model<ICoupon>("Coupon", couponSchema);
 
